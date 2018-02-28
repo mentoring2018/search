02e4f5084aa78a56639ba3c7c3d7d65a5fb2f9a3
jingerbread <qwadrillion@gmail.com>
2018-02-28 16:53:47 +0300

    [initial commit]

--- src/main/java/com/jingerbread/DictionaryLoader.java
+++ src/main/java/com/jingerbread/DictionaryLoader.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -20,9 +21,11 @@ public class DictionaryLoader {
 
     public DictionaryLoader() {
         ClassLoader classLoader = getClass().getClassLoader();
-        InputStream in = classLoader.getResourceAsStream(wordsPath);
-        words = new BufferedReader(new InputStreamReader(in,
+        try (InputStream in = classLoader.getResourceAsStream(wordsPath)) {
+            words = new BufferedReader(new InputStreamReader(in,
                     StandardCharsets.UTF_8)).lines().collect(Collectors.toList());
-
+        } catch (IOException e) {
+            log.error("Error occurred", e);
+        }
     }
 }
